1af42cb4402382b7e89ea4a1ff303b778f9cd041
sponsnordj <nwraldynbnywsf67@gmail.com>
2026-07-23 15:02:54 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
+function showSection(sectionId){
 
+document.getElementById("home-page").style.display="none";
+
+let sections=document.querySelectorAll(".section-page");
+
+sections.forEach(function(section){
+section.style.display="none";
+});
+
+document.getElementById(sectionId).style.display="block";
+
+}
+
+function goBack(){
+
+let sections=document.querySelectorAll(".section-page");
+
+sections.forEach(function(section){
+section.style.display="none";
+});
+
+document.getElementById("home-page").style.display="block";
+}
